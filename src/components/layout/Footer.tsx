a6c4df300dcde19d52e696973f51a8d6d7f21bc3
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-22 21:03:23 +0300

    feat: quote button

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -16,20 +16,30 @@ function Footer(): JSX.Element {
           </p>
           <section className="mt-5 flex w-full flex-wrap items-center justify-center gap-5">
             <Link
-              href="/iletisim"
-              className="flex items-center justify-center gap-2 border border-black/30 bg-white px-3 py-1 text-sm text-zinc-800 hover:bg-black hover:text-white"
+              href="/contact"
+              className="flex items-center justify-center gap-2 border border-black/30 bg-white px-3 py-1 text-sm text-zinc-800 transition-all duration-150 hover:bg-black hover:text-white"
             >
               <BsEnvelopeFill className="h-4 w-4" />
               <span>{t('footer.contact-button')}</span>
             </Link>
             <a
               href="tel:+90 212 486 37 73"
-              className="flex items-center justify-center gap-2 border border-black/30 bg-white px-3 py-1 text-sm text-zinc-800 hover:bg-black hover:text-white"
+              className="flex items-center justify-center gap-2 border border-black/30 bg-white px-3 py-1 text-sm text-zinc-800 transition-all duration-150 hover:bg-black hover:text-white"
             >
               <BsFillTelephoneFill className="h-4 w-4" />
               <span>+90 212 486 37 73</span>
             </a>
           </section>
+          <Link
+            href="/contact"
+            className="relative mt-5 border border-black bg-black px-4 py-2 text-lg font-medium text-white shadow-lg shadow-black/50 transition-all duration-150 hover:bg-white hover:text-black"
+          >
+            <span className="absolute top-0 right-0 flex h-4 w-4 -translate-y-1/2 translate-x-1/2 items-center justify-center">
+              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-black opacity-75" />
+              <span className="relative inline-flex h-4 w-4 rounded-full border border-white bg-black" />
+            </span>
+            {t('footer.quote')}
+          </Link>
         </section>
       </section>
       <footer className="m-0 flex min-h-[100px] w-full flex-wrap items-center justify-center bg-black p-0 font-theme">
